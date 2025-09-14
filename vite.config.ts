d363e1792c5b309025e754ae4d98995d8adaa31d
Akhil Harshavardhan <192791014+Akhil-a007@users.noreply.github.com>
2025-09-14 22:37:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['pdfjs-dist/webpack'],
+      external: ['pdfjs-dist/build/pdf'],
     },
   },
-})
+});
